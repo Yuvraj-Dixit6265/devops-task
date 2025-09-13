f55a0f1e4a467f0b020ac1cc578bf8215fa84f7a
Yuvraj Dixit <yuvrajdixit@Yuvrajs-MacBook-Air.local>
2025-09-14 02:21:21 +0530

    sdd

--- app/app.js
+++ app/app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// return the PNG at root
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'logoswayatt.png'));
 });
 
-const PORT = process.env.PORT || 8080;      // Cloud Run sets PORT=8080
-app.listen(PORT, '0.0.0.0', () => {         // bind 0.0.0.0 for Docker/Cloud Run
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on port ${PORT}`);
 });
